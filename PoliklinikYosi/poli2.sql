9c9bbc05c2aa463bbd0ede4f2250320551481fe5
yosefinojulianto <150247409+yosefinojulianto@users.noreply.github.com>
2024-01-26 00:05:49 +0700

    Add files via upload

---
+++ PoliklinikYosi/poli2.sql
@@ -0,0 +1,380 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Jan 2024 pada 22.28
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.3.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poli2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_poli`
+--
+
+CREATE TABLE `daftar_poli` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `id_pasien` int(11) UNSIGNED NOT NULL,
+  `id_jadwal` int(11) UNSIGNED NOT NULL,
+  `keluhan` text NOT NULL,
+  `no_antrian` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `daftar_poli`
+--
+
+INSERT INTO `daftar_poli` (`id`, `id_pasien`, `id_jadwal`, `keluhan`, `no_antrian`) VALUES
+(4, 1, 1, 'Sakit gigi', 1),
+(5, 2, 1, 'Sakit Mata', 2),
+(6, 1, 4, 'Sakit punggung', 1),
+(7, 7, 5, 'susah bab', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_periksa`
+--
+
+CREATE TABLE `detail_periksa` (
+  `id` int(11) NOT NULL,
+  `id_periksa` int(11) UNSIGNED NOT NULL,
+  `id_obat` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `detail_periksa`
+--
+
+INSERT INTO `detail_periksa` (`id`, `id_periksa`, `id_obat`) VALUES
+(1, 1, 2),
+(2, 2, 3),
+(3, 3, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(50) DEFAULT NULL,
+  `id_poli` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `dokter`
+--
+
+INSERT INTO `dokter` (`id`, `nama`, `alamat`, `no_hp`, `id_poli`) VALUES
+(1, 'Dr.Yosi', 'panggang', '0895413425425', 2),
+(2, 'Dr. intan', 'semarang', '0832424564361', 1),
+(3, 'Dr. mark', 'kudus', '086554552110', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal_periksa`
+--
+
+CREATE TABLE `jadwal_periksa` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `id_dokter` int(11) UNSIGNED NOT NULL,
+  `hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu') NOT NULL,
+  `jam_mulai` time NOT NULL,
+  `jam_selesai` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jadwal_periksa`
+--
+
+INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`) VALUES
+(1, 1, 'Senin', '07:00:00', '20:00:00'),
+(2, 1, 'Jumat', '07:00:00', '20:00:00'),
+(3, 2, 'Selasa', '08:00:00', '21:00:00'),
+(4, 2, 'Kamis', '08:00:00', '21:00:00'),
+(5, 3, 'Rabu', '09:00:00', '21:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `obat`
+--
+
+CREATE TABLE `obat` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nama_obat` varchar(50) NOT NULL,
+  `kemasan` varchar(35) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `obat`
+--
+
+INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`) VALUES
+(1, 'Paracetamol', 'Tablet', 9000),
+(2, 'Asam Folat tablet 1 mg', 'btl 100 tablet', 6500),
+(3, 'Insto', 'Botol', 12000),
+(4, 'Saleb 88', 'Botol', 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_ktp` varchar(255) NOT NULL,
+  `no_hp` varchar(50) NOT NULL,
+  `no_rm` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `nama`, `alamat`, `no_ktp`, `no_hp`, `no_rm`) VALUES
+(1, 'sunny', 'Jalan pakudewa', '332452346546435', '02199303765323', '202401-001'),
+(2, 'umur', 'semarang', '33241111424534563', '087389231930', '202401-002'),
+(7, 'yosi', 'panggang', '33366457472432', '089545346457', '202401-003');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `periksa`
+--
+
+CREATE TABLE `periksa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_daftar_poli` int(11) UNSIGNED NOT NULL,
+  `tgl_periksa` datetime NOT NULL,
+  `catatan` text NOT NULL,
+  `biaya_periksa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `id_daftar_poli`, `tgl_periksa`, `catatan`, `biaya_periksa`) VALUES
+(1, 4, '2024-01-26 08:30:00', 'Pijat ++', 100000),
+(2, 5, '2024-01-26 09:30:00', 'Memberi Obat Tetes Mata', 45000),
+(3, 7, '2024-01-24 13:00:00', 'Memecahkan bisul 7 biji secara bersamaan', 150000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `poli`
+--
+
+CREATE TABLE `poli` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nama_poli` varchar(25) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `poli`
+--
+
+INSERT INTO `poli` (`id`, `nama_poli`, `keterangan`) VALUES
+(1, 'poli anak', 'khusus anak kecil'),
+(2, 'poli umum', 'untuk masyarakat umum');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','dokter','pasien') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(0, 'yosi', '$2y$10$iDKMNt4pax35VGgWiVj67u536aa4DwyjDI/ppuqjJOOPIwwR6NOJu', 'dokter'),
+(0, 'admin', '$2y$10$K7RuWCeFyNbQo9R0ypRW5O6l9edaUnJnrqlmOpYq8fbtVnVqr09RC', 'admin'),
+(0, 'pasien', '$2y$10$PWea98PiFgTmvEB9lfWdnOvHLhrGj0ftMaQKCVuEEUPtNRoWoCKOK', 'pasien');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_daftar_poli_pasien` (`id_pasien`),
+  ADD KEY `fk_daftar_poli_dokter` (`id_jadwal`);
+
+--
+-- Indeks untuk tabel `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_detail_periksa_obat` (`id_obat`),
+  ADD KEY `fk_detail_periksa_periksa` (`id_periksa`);
+
+--
+-- Indeks untuk tabel `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_dokter_poli` (`id_poli`);
+
+--
+-- Indeks untuk tabel `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_jadwal_periksa_dokter` (`id_dokter`);
+
+--
+-- Indeks untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `periksa`
+--
+ALTER TABLE `periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_periksa_daftar_poli` (`id_daftar_poli`);
+
+--
+-- Indeks untuk tabel `poli`
+--
+ALTER TABLE `poli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `periksa`
+--
+ALTER TABLE `periksa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `poli`
+--
+ALTER TABLE `poli`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  ADD CONSTRAINT `fk_daftar_poli_dokter` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal_periksa` (`id`),
+  ADD CONSTRAINT `fk_daftar_poli_pasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD CONSTRAINT `fk_detail_periksa_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_detail_periksa_periksa` FOREIGN KEY (`id_periksa`) REFERENCES `periksa` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `dokter`
+--
+ALTER TABLE `dokter`
+  ADD CONSTRAINT `fk_dokter_poli` FOREIGN KEY (`id_poli`) REFERENCES `poli` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  ADD CONSTRAINT `fk_jadwal_periksa_dokter` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `periksa`
+--
+ALTER TABLE `periksa`
+  ADD CONSTRAINT `fk_periksa_daftar_poli` FOREIGN KEY (`id_daftar_poli`) REFERENCES `daftar_poli` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
